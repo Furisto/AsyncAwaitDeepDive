7d6fbaf9fb969e2f4b6f44b22c727f7056d7e9c2
Furisto <24721048+Furisto@users.noreply.github.com>
2019-04-02 21:41:09 +0200

    Rename scenario

--- AsyncAwait.DeepDive.Scenarios/Scenarios.TaskCompletionSource/Program.cs
+++ AsyncAwait.DeepDive.Scenarios/Scenarios.TaskCompletionSource/Program.cs
@@ -1,12 +1,11 @@
-﻿using Scenarios.TaskCompletionSourceHang;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace TaskCompletionSource2
+namespace TaskCompletionSource
 {
     class Program
     {
